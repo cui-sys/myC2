852a305cb002f579ca5e26c711f205110035b990
OKme <95911472+cui-sys@users.noreply.github.com>
2022-04-06 22:03:27 +0800

    Add files via upload

---
+++ test_2022_4_6_03.h
@@ -0,0 +1,14 @@
+#ifndef TEST_2022_4_6_03_H//一个 .C 文件 对应一个头文件
+#define TEST_2022_4_6_03_H
+#if __cplusplus//
+extern "C"//
+{
+#endif//混合编程头文件的声明 关键
+
+    extern int my_add(int x, int y);
+    extern int my_sub(int x, int y);
+
+#if __cplusplus
+}
+#endif
+#endif // TEST_2022_4_6_03_H
