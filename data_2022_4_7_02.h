c292c00b0d80afdc9f60bca222c32d97cd7bdc3a
OKme <95911472+cui-sys@users.noreply.github.com>
2022-04-08 06:11:23 +0800

    Add files via upload

---
+++ data_2022_4_7_02.h
@@ -0,0 +1,16 @@
+#ifndef DATA_H
+#define DATA_H
+
+
+class Data
+{
+private:
+    int num;
+public:
+    //…Ë÷√num
+    void setNum(int data);
+    //∂¡»°num
+    int getNum(void);
+};
+
+#endif // DATA_H
